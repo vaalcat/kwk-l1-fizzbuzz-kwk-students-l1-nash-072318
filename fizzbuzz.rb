0fd8078b7649d4c2c3acabef4329cbcadca7e1a9
vaalcat <val.cat456@icloud.com>
2018-07-24 18:50:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,10 @@
 number = 15 
-
-if number%3 == 0  
-  
+if number%5 && number%3 == 0 
+  puts "Fizzbuzz"
+elsif number%3 == 0  
   puts "Fizz"
 elsif number%5 == 0 
   puts "Buzz" 
-elsif number%5 && number%3 == 0 
+else ! () 
+  
   
